d5b55e652b9b8ff9a0669973c105f5f4c6c2a4dc
Daniel Leal Pimenta <129066504+daniellealpimenta@users.noreply.github.com>
2025-10-14 13:51:53 -0300

    Adding the base

---
+++ Challenge09/Challenge09App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge09App.swift
+//  Challenge09
+//
+//  Created by Daniel Leal PImenta on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge09/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge09
+//
+//  Created by Daniel Leal PImenta on 14/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
