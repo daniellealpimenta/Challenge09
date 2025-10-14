01c9ad49d7dffcfb18b08f781fb38766a6b8c3ae
Daniel Leal Pimenta <129066504+daniellealpimenta@users.noreply.github.com>
2025-10-14 17:35:40 -0300

    Adjusting the location with WeatherKit

--- Challenge09/Views/ContentView.swift
+++ Challenge09/Views/ContentView.swift
@@ -14,46 +14,66 @@ import CoreLocation
 struct ContentView: View {
     @Environment(LocationManager.self) var locationManager
     @State private var selectedCity: City?
+    
     let weatherManager = WeatherManager.shared
     @State private var weather: Weather?
     @State private var isLoading = false
     
     var body: some View {
         VStack {
-            if isLoading {
-                ProgressView()
-                Text("Fazendo requisição do Clima...")
-            } else {
-                if let weather {
-                    Text(Date.now.formatted(date: .abbreviated, time: .omitted))
-                    Text(Date.now.formatted(date: .omitted, time: .shortened))
-                    Image(systemName: weather.currentWeather.symbolName)
-                        .renderingMode(.original)
-                        .symbolVariant(.fill)
-                        .font(.system(size: 60.0, weight: .bold))
+            if let selectedCity {
+                if isLoading {
+                    ProgressView()
+                    Text("Fazendo requisição do Clima...")
+                } else {
+                    Text(selectedCity.name)
+                        .font(.title)
                         .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 20)
-                                .fill(.secondary.opacity(0.2))
-                        )
-                    let temp = weatherManager.temperatureFormatter.string(from: weather.currentWeather.temperature)
-                    Text(temp)
-                        .font(.title2)
-                    Text(weather.currentWeather.condition.description)
-                        .font(.title3)
-                    AttributionView()
-                    Text(weatherManager.weatherDays.count.description + " dias de previsão")
+                    
+                    if let weather {
+                        Text(Date.now.formatted(date: .abbreviated, time: .omitted))
+                        Text(Date.now.formatted(date: .omitted, time: .shortened))
+                        Image(systemName: weather.currentWeather.symbolName)
+                            .renderingMode(.original)
+                            .symbolVariant(.fill)
+                            .font(.system(size: 60.0, weight: .bold))
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(.secondary.opacity(0.2))
+                            )
+                        let temp = weatherManager.temperatureFormatter.string(from: weather.currentWeather.temperature)
+                        Text(temp)
+                            .font(.title2)
+                        Text(weather.currentWeather.condition.description)
+                            .font(.title3)
+                        AttributionView()
+                        Text(weatherManager.weatherDays.count.description + " dias de previsão")
+                    }
                 }
             }
         }
         .padding()
-        .task {
-            isLoading = true
-            Task.detached { @MainActor in
-                weather = await weatherManager.fetchWeather(for: CLLocation(latitude: -15.79284, longitude: -47.88249))
+        .task(id: locationManager.currentLocation) {
+            if let currentLocation = locationManager.currentLocation, selectedCity == nil {
+                selectedCity = currentLocation
             }
-            isLoading = false
         }
+        
+        .task(id: selectedCity) {
+            if let selectedCity {
+                await fetchWeatherView(for: selectedCity)
+            }
+        }
+            
+    }
+    
+    func fetchWeatherView(for city: City) async {
+        isLoading = true
+        Task.detached { @MainActor in
+            weather = await weatherManager.fetchWeather(for: city.clLocation)
+        }
+        isLoading = false
     }
 }
 
