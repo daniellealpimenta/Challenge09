a045a765414263aca7c47ae8a7942164a2acafb2
FilipiNyetz <romao.martins.filipi@gmail.com>
2025-10-14 17:13:34 -0300

    formatted date

--- Challenge09/Views/ContentView.swift
+++ Challenge09/Views/ContentView.swift
@@ -41,6 +41,7 @@ struct ContentView: View {
                         .font(.title3)
                     AttributionView()
                     Text(weatherManager.weatherDays.count.description + " dias de previsão")
+                    
                 }
             }
         }
